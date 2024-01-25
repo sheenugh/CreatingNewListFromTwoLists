da0452d0b046d1661d87b51136688215415504f8
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-26 01:18:16 +0800

    pseudo code

--- creating_new_set_from_two_sets.py
+++ creating_new_set_from_two_sets.py
@@ -1,5 +1,6 @@
 
-Given:
-list1 = [10, 20, 25, 30, 35]
-list2 = [40, 45, 60, 75, 90]
-#Since the two given list consist of both even numbers, I'll be creating a new two list on my own
+# ----- VARIABLES -----
+
+# >>>>>>>>>> PSEUDO CODE <<<<<<<<<
+# - Given List
+# - This section is for the for loop
--- trying_on_my_own.py
+++ trying_on_my_own.py
@@ -13,11 +13,11 @@
 
 # Since the two given list consist of both even numbers, I'll be creating a new two list on my own
 
-list1 = [10, 23, 27, 30, 31] #getting odd numbers to the new list
-list2 = [40, 45, 63, 75, 91] #getting even numbers to the new list
+list1 = [10, 23, 27, 30, 31] #getting odd numbers in this list and transfer to the new list
+list2 = [40, 45, 63, 75, 91] #getting even numbers in this list and transfer to the new list
 
 for i in list1:
     pass
 for j in list2:
     pass
-print("New List = ", list1[1], list1[2], list1[4], list2[1], list2[0], list2[3])
+print("New or Result List = ", [list1[1], list1[2], list1[4], list2[1], list2[0], list2[3]])
