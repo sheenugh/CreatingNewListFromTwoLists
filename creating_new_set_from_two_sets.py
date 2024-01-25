496ad640bb23094c73406b987a8f56f1955cd686
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-26 01:10:37 +0800

    oh diba gumana yong utak ko tru integration ng mga natutunan ko

---
+++ creating_new_set_from_two_sets.py
@@ -0,0 +1,5 @@
+
+Given:
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
+#Since the two given list consist of both even numbers, I'll be creating a new two list on my own
--- trying_on_my_own.py
+++ trying_on_my_own.py
@@ -2,3 +2,22 @@
 # searching it to the internet or chatgpt.
 
 # Note: I watch the tutorial first in YT about how to use the specific function and apply it here in my task
+
+
+# ------------------------------------------------------
+
+
+# Given:
+# list1 = [10, 20, 25, 30, 35]
+# list2 = [40, 45, 60, 75, 90]
+
+# Since the two given list consist of both even numbers, I'll be creating a new two list on my own
+
+list1 = [10, 23, 27, 30, 31] #getting odd numbers to the new list
+list2 = [40, 45, 63, 75, 91] #getting even numbers to the new list
+
+for i in list1:
+    pass
+for j in list2:
+    pass
+print("New List = ", list1[1], list1[2], list1[4], list2[1], list2[0], list2[3])
