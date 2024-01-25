705c50ce47ffdd76410f96f1b8ae82c388d3ceab
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-26 00:50:35 +0800

    added word to the 'trying on..' py file

--- trying_on_my_own.py
+++ trying_on_my_own.py
@@ -1,4 +1,4 @@
-# This py file is trying on my own first, wherein I am testing if I can solve the exercise on my own like creating my own algorithm without
+# This py file is trying on my own first, wherein I am testing myself if I can solve the exercise on my own like creating my own algorithm without
 # searching it to the internet or chatgpt.
 
 # Note: I watch the tutorial first in YT about how to use the specific function and apply it here in my task
