dc662bb6275496ed255208f0b2f2a97d6b011e3e
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-26 01:20:26 +0800

    transferring the code to the main py

--- creating_new_set_from_two_sets.py
+++ creating_new_set_from_two_sets.py
@@ -3,4 +3,17 @@
 
 # >>>>>>>>>> PSEUDO CODE <<<<<<<<<
 # - Given List
+    # Given:
+    # list1 = [10, 20, 25, 30, 35]
+    # list2 = [40, 45, 60, 75, 90]
+    # Since the two given list consist of both even numbers, I'll be creating a new two list on my own.
+
+list1 = [10, 23, 27, 30, 31] #getting odd numbers in this list and transfer to the new list.
+list2 = [40, 45, 63, 75, 91] #getting even numbers in this list and transfer to the new list.
+
 # - This section is for the for loop
+for i in list1:
+    pass
+for j in list2:
+    pass
+print("New or Result List = ", [list1[1], list1[2], list1[4], list2[1], list2[0], list2[3]])
